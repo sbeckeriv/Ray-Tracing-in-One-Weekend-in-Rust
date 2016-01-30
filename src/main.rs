c9d20045ea3f0f37c19e2d3fac4fe9857ef80a58
Stephen Becker IV <github@deathbyescalator.com>
2016-01-30 12:17:35 -0800

    demo image

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,20 @@ use std::path::Path;
 use num::complex::Complex;
 
 fn main() {
-
-    let imgx = 800;
-    let imgy = 800;
+    let image_x = 200;
+    let image_y = 100;
 
     // Create a new ImgBuf with width: imgx and height: imgy
-    let mut imgbuf = image::ImageBuffer::new(imgx, imgy);
+    let mut imgbuf = image::ImageBuffer::new(image_x, image_y);
 
     // Iterate over the coordiantes and pixels of the image
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        *pixel = image::Rgba([255, 0, 0, 0]);
-
+        let r = x as f32 / image_x as f32;
+        let g = (image_y - 1 - y) as f32 / image_y as f32;
+        let b = 0.2;
+        let base = 255.99;
+        println!("{:?}", (x, image_y - 1 - y));
+        *pixel = image::Rgba([(base * r) as u8, (base * g) as u8, (base * b) as u8, 0]);
     }
     // Save the image as “fractal.png”
     let ref mut fout = File::create(&Path::new("fractal.ppm")).unwrap();
