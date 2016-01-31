f26ff88a4cd8fd59d4cbcebf83c37e26fc15acd0
Stephen Becker IV <github@deathbyescalator.com>
2016-01-31 11:15:53 -0800

    chapter 5 done
    Shocked but its working.

--- src/main.rs
+++ src/main.rs
@@ -8,31 +8,16 @@ use nalgebra::Dot;
 mod ray;
 use ray::Ray;
 mod objects;
+use objects::{HitableList, Sphere, HitRecord};
 
-fn hit_sphere(center: &Vec3<f32>, radius: f32, ray: &Ray) -> Option<f32> {
-    let origin = ray.origin;
-    let direction = ray.direction;
-    let oc = origin - *center;
-    let a = direction.dot(&direction);
-    let b = 2.0 * oc.dot(&direction);
-    let c = oc.dot(&oc) - radius*radius;
-    let discriminate = b * b - 4.0 * a * c;
-    //println!("{:?} {},{},{}",discriminate,a,b,c);
-    if discriminate < 0.0{
-        None
-    }else {
-        Some((0.0-b - discriminate.sqrt())/(2.0*a))
-    }
-}
-
-fn color(ray: &ray::Ray) -> Vec3<f32> {
+fn color(ray: &Ray, world: &HitableList) -> Vec3<f32> {
     let direction: Vec3<f32> = ray.direction;
-    let sphere = Vec3::new(0.0,0.0,0.0-1.0);
-    match hit_sphere(&sphere, 0.5, ray){
-        Some(t) =>{
-            let n = ray.point_at_parameter(t)-Vec3::new(0.0, 0.0, 0.0-1.0) ;
-            Vec3::new(n.x+1.0, n.y+1.0, n.z+1.0) * 0.5
-        },
+    let sphere = Vec3::new(0.0, 0.0, 0.0 - 1.0);
+    match world.hit(ray, &0.0, &std::f32::MAX) {
+        Some(t) => {
+            let n = t.normal;
+            Vec3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0) * 0.5
+        }
         None => {
             let t = 0.5 * (direction.y + 1.0);
             Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
@@ -48,6 +33,9 @@ fn main() {
     let vertical = Vec3::new(0.0, 2.0, 0.0);
     let origin = Vec3::new(0.0, 0.0, 0.0);
 
+    let mut world = HitableList::new();
+    world.push(Sphere::new(Vec3::new(0.0, 0.0, 0.0 - 1.0), 0.5));
+    world.push(Sphere::new(Vec3::new(0.0, 0.0 - 100.5, 0.0 - 1.0), 100.0));
     // Create a new ImgBuf with width: imgx and height: imgy
     let mut imgbuf = image::ImageBuffer::new(image_x, image_y);
 
@@ -56,7 +44,7 @@ fn main() {
         let u = x as f32 / image_x as f32;
         let v = (image_y - 1 - y) as f32 / image_y as f32;
         let ray = Ray::new(origin, lower_left_corner + horizon * u + vertical * v);
-        let col = color(&ray);
+        let col = color(&ray, &world);
         let base = 255.99;
         *pixel = image::Rgba([(base * col.x) as u8, (base * col.y) as u8, (base * col.z) as u8, 0]);
     }
