ceb24cf22a61ba07556ebe4f1fa27eff29b483a5
Stephen Becker IV <github@deathbyescalator.com>
2016-01-31 16:01:51 -0800

    diffusing step 1

--- src/main.rs
+++ src/main.rs
@@ -11,17 +11,34 @@ use nalgebra::Dot;
 mod ray;
 use ray::Ray;
 mod objects;
-use objects::{HitableList, Sphere, HitRecord};
+use objects::{HitableList, Sphere};
 mod camera;
 use camera::Camera;
 
-fn color(ray: &Ray, world: &HitableList) -> Vec3<f32> {
+fn random_in_unit_sphere(rand: &mut StdRng) -> Vec3<f32> {
+    let random_index = Range::new(0.0, 1.0);
+    let mut p: Vec3<f32>;
+    let minus_vec = Vec3::new(1.0, 1.0, 1.0);
+    loop {
+        p = Vec3::new(random_index.ind_sample(rand),
+        random_index.ind_sample(rand),
+        random_index.ind_sample(rand)) * 2.0 - minus_vec;
+        if p.dot(&p) < 1.0 {
+            break;
+        }
+    }
+    p
+}
+
+fn color(ray: &Ray, world: &HitableList, rand: &mut StdRng) -> Vec3<f32> {
     let direction: Vec3<f32> = ray.direction;
     let sphere = Vec3::new(0.0, 0.0, 0.0 - 1.0);
     match world.hit(ray, &0.0, &std::f32::MAX) {
         Some(t) => {
             let n = t.normal;
-            Vec3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0) * 0.5
+            let target = t.normal + random_in_unit_sphere(rand) + t.p;
+            //Vec3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0) * 0.5
+            color(&Ray::new(t.p, target-t.p), world, rand)*0.5
         }
         None => {
             let t = 0.5 * (direction.y + 1.0);
@@ -41,7 +58,7 @@ fn main() {
     let horizon = Vec3::new(4.0, 0.0, 0.0);
     let vertical = Vec3::new(0.0, 2.0, 0.0);
     let origin = Vec3::new(0.0, 0.0, 0.0);
-    let camera = Camera::new(origin, lower_left_corner,vertical, horizon);
+    let camera = Camera::new(origin, lower_left_corner, vertical, horizon);
 
     let mut world = HitableList::new();
     world.push(Sphere::new(Vec3::new(0.0, 0.0, 0.0 - 1.0), 0.5));
@@ -51,14 +68,14 @@ fn main() {
 
     // Iterate over the coordiantes and pixels of the image
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let mut col = Vec3::new(0.0,0.0,0.0);
-        for i in 0..ns{
+        let mut col = Vec3::new(0.0, 0.0, 0.0);
+        for _ in 0..ns {
             let rand_x = random_index.ind_sample(&mut rng);
             let rand_y = random_index.ind_sample(&mut rng);
             let u = (x as f32 + rand_x) / image_x as f32;
-            let v = ((image_y - 1 - y) as f32+rand_y) / image_y as f32;
+            let v = ((image_y - 1 - y) as f32 + rand_y) / image_y as f32;
             let ray = camera.get_ray(&u, &v);
-            col = col+ color(&ray, &world);
+            col = col + color(&ray, &world, &mut rng);
         }
         let base = 255.99;
         col = col / ns as f32;
